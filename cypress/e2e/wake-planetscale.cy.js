7c8ea6b5c2fd862fae6114f3fddd4cfb65a470cc
Andre Fong <fongandre123@gmail.com>
2023-04-19 19:05:29 -0400

    fix: Signin and signout w Cypress

--- cypress/e2e/wake-planetscale.cy.js
+++ cypress/e2e/wake-planetscale.cy.js
@@ -1,11 +1,22 @@
 describe("Wake Planetscale DB", () => {
   it("Visits UniSpaces and wakes database", () => {
+    cy.intercept("/api/sessions").as("session");
+
     cy.visit("https://uni-spaces.vercel.app/");
-    cy.get("input").clear().type("scar");
+    
+    cy.get("nav").contains("Sign in").click();
+    cy.get("#username").clear().type("test");
+    cy.get("#password").clear().type("test123");
+    cy.contains("button", "Confirm").click();
+    cy.contains("Successfully signed in");
+
+    cy.wait(5000);
+    cy.wait("@session");
     cy.wait(3000);
-    cy.contains("University of Toronto (Scarborough)").should(
-      "exist.and.be.visible"
-    );
-    cy.contains("University of Toronto (St. George)").should("not.exist");
+
+    cy.get(`img[alt="test's profile"]`).click();
+    cy.contains("Sign out").click();
+
+    cy.wait(1000);
   });
 });
